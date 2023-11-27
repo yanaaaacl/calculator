33b3c762abd1fb0999e402edc743ee5bc953ed5c
Yana Chackhovskaya <a9506862137@yandex.ru>
2023-11-27 14:50:02 +0300

    designed

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let input = document.querySelector('#calculator__box-answer');
+let number = document.querySelector('.calculator__buttons-number');
+
+let clear = document.querySelector('.clear');
+let plus = document.querySelector('.plus');
+let minus = document.querySelector('.minus');
+let multiply = document.querySelector('.multiply');
+let division = document.querySelector('.division');
+let comma = document.querySelector('.comma');
+let proc = document.querySelector('.proc');
+let equally = document.querySelector('.equally');
