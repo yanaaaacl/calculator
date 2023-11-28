efe4f7b2bc30e671b75c19fb7ea665e80aed0529
Yana Chackhovskaya <a9506862137@yandex.ru>
2023-11-28 18:51:06 +0300

    numbers click

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let input = document.querySelector('#calculator__box-answer');
-let number = document.querySelector('.calculator__buttons-number');
+let number = document.querySelectorAll('.calculator__buttons-number');
 
 let clear = document.querySelector('.clear');
 let plus = document.querySelector('.plus');
@@ -9,3 +9,16 @@ let division = document.querySelector('.division');
 let comma = document.querySelector('.comma');
 let proc = document.querySelector('.proc');
 let equally = document.querySelector('.equally');
+
+
+
+
+    number.forEach(function(el){
+      let value = el;
+      let text = Number(value.textContent);
+
+      value.addEventListener('click', function(){
+            input.value += text;
+      })
+      
+    });
