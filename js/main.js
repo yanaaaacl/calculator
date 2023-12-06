ab1755c9d898ad9cbe979ece5cd52ae255def503
Yana Chackhovskaya <a9506862137@yandex.ru>
2023-12-06 16:53:26 +0300

    result

--- js/main.js
+++ js/main.js
@@ -9,104 +9,85 @@ input.textContent = "0";
 
 let number = document.querySelectorAll('.calculator__buttons-number');
 number.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (operator === "=") {
-      operator = null;
+  button.addEventListener('click', function () {
+    if (operator === '=') {
       firstNumber = null;
+      operator = null;
       secondNumber = null;
     }
-
     if (operator === null) {
-     
       if (firstNumber === null) {
         firstNumber = button.textContent;
       } else {
-        if (firstNumber.includes(".") && button.textContent === ".") {
-          return;
+        if (firstNumber != null) {
+          firstNumber += button.textContent;
         }
-        if (firstNumber.length > 10) {
-          return;
-        }
-        firstNumber += button.textContent;
       }
-   
       input.textContent = firstNumber;
     } else {
-     
       if (secondNumber === null) {
         secondNumber = button.textContent;
       } else {
-        if (secondNumber.includes(".") && button.textContent === ".") {
-          return;
+        if (secondNumber !== null) {
+          secondNumber += button.textContent;
         }
-        if (secondNumber.length > 10) {
-          return;
-        }
-        secondNumber += button.textContent;
       }
-     
       input.textContent = secondNumber;
     }
-  });
-});
+  })
+})
+
 
 
 let clear = document.querySelector('.clear');
-clear.addEventListener("click", () => {
-  operator = null;
+clear.addEventListener('click', function () {
   firstNumber = null;
+  operator = null;
   secondNumber = null;
   input.textContent = "0";
-});
+})
+
 
 
 let sym = document.querySelectorAll('.symbols');
 sym.forEach((button) => {
-  button.addEventListener("click", () => {
-    if ((firstNumber === null && secondNumber === null) || operator === "=") {
-      return;
-    }
-
-    if (firstNumber !== null && secondNumber === null) {
-      
+  button.addEventListener('click', function(){
+    if (firstNumber !== null && secondNumber === null){
       operator = button.textContent;
-      input.textContent = firstNumber;
-    } 
-  });
-});
+    }
+  })
+})
+
 
 let equally = document.querySelector('.equally');
-equally.addEventListener("click", () => {
-  if ((firstNumber === null && secondNumber === null) || operator === "=") {
-    return;
-  }
-
-  if (firstNumber !== null && secondNumber !== null) {
+equally.addEventListener('click', function(){
+  if (firstNumber !== null && secondNumber !== null){
     let result;
-    switch (operator) {
-      case "+":
+    switch (operator){
+      case '+':
         result = parseFloat(firstNumber) + parseFloat(secondNumber);
         break;
-      case "-":
+      case '-':
         result = parseFloat(firstNumber) - parseFloat(secondNumber);
         break;
-      case "*":
+      case '*':
         result = parseFloat(firstNumber) * parseFloat(secondNumber);
         break;
-      case "/":
-        if (secondNumber === "0") {
-          input.textContent = "ERROR";
+      case '/':
+        if (secondNumber === '0'){
+          input.textContent = 'Error';
           break;
         }
         result = parseFloat(firstNumber) / parseFloat(secondNumber);
         break;
     }
-    input.textContent = result;
+    
     firstNumber = result.toString();
+    input.textContent = firstNumber;
     secondNumber = null;
-    operator = null;
   }
-});
+})
+
 
 
 
